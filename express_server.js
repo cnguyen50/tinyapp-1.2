ed053134243477691d262e31ba01883988452095
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-17 23:18:11 +0000

    Created Route for /urls/shortURL

--- express_server.js
+++ express_server.js
@@ -10,26 +10,38 @@ const urlDatabase = {
 //setting up EJS template engine
 app.set("view engine", "ejs");
 
+
 //urlsDatabase pairs on the browser
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
-//
+
+  
+  //TinyURL: longURL shortURL: shortURL
+  app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+  });
+
+//short/long urls with stylesheet 
 app.get("/urls", (req, res) => {
     let templateVars = { urls : urlDatabase };
     res.render("urls_index", templateVars);
 });
+
 
 //Hello World on the browser
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });  
 
+
 //Hello! on browser
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
