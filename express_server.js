6dbead7fac9f00f015323655651ba12e36754ee9
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-17 19:11:02 +0000

    Added EJS as template engine

--- express_server.js
+++ express_server.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//setting template engine
+app.set("view engine", "ejs");
+
 //urlsDatabase pairs on the browser
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
+//Hello World on the browser
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });  
 
+//Hello! on browser
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
