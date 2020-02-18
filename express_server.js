faf6975e32259eaf071c82865b9d4404408d0f68
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-18 00:13:31 +0000

    Handled route for /urls/new and required body parser

--- express_server.js
+++ express_server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//setting up EJS template engine
+app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-//setting up EJS template engine
-app.set("view engine", "ejs");
-
 
 //urlsDatabase pairs on the browser
 app.get("/urls.json", (req, res) => {
@@ -17,12 +20,16 @@ app.get("/urls.json", (req, res) => {
   });
 
 
-  
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
+
   //TinyURL: longURL shortURL: shortURL
-  app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-  });
+});
 
 //short/long urls with stylesheet 
 app.get("/urls", (req, res) => {
