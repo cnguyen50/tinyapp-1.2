7a8840a731a21495401e0b56f93af5843813decf
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-20 22:34:43 +0000

    moved checkEmail function and exported

---
+++ helpers.js
@@ -0,0 +1,12 @@
+const checkEmail = function(email, database) {
+    for (let id in database) {
+        if (database[id].email === email) {
+            return database[id];
+        }
+    }
+    return undefined;
+};
+
+
+
+module.exports =  checkEmail;
