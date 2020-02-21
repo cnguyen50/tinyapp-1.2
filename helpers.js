e1d927754c906ecca69aca1a85272cd4893dc059
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-21 00:49:25 +0000

    moved urlsForuser and generate function and exported

--- helpers.js
+++ helpers.js
@@ -7,6 +7,26 @@ const checkEmail = function(email, database) {
   return undefined;
 };
 
+const urlsForUser = function(id, database) {
+  let match = {};
+  for (const key in database) {
+    if (database[key].userID === id) {
+      match[key] = database[key].longURL;
+    }
+  }
+  return match;
+};
 
 
-module.exports = {checkEmail};
+const generateRandomString = function() {
+  let result = "";
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = chars.length;
+  for (let i = 1; i < 7; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+
+module.exports = { checkEmail,urlsForUser, generateRandomString };
