db928e1dae376bc74a1ed3e0d14253329be02602
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-21 00:20:16 +0000

    took out dummy data

--- express_server.js
+++ express_server.js
@@ -16,10 +16,7 @@ app.use(cookieSession({
 
 const users = {};
 
-const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
-};
+const urlDatabase = {};
 
 
 const urlsForUser = function(id) {
